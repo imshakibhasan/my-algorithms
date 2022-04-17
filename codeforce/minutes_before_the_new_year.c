4bbd895dc5d394c047b90b657d41421059462de1
Shakibul Hasan <hasan29shakib@gmail.com>
2022-04-17 14:40:33 +0600

    wip: minutes before the new year

---
+++ codeforce/minutes_before_the_new_year.c
@@ -0,0 +1,12 @@
+/**
+ * @file minutes_before_the_new_year.c
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * problem: https://codeforces.com/problemset/problem/1283/A
+ * 
+ */
