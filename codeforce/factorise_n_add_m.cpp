2cdc0fff8a4b70e9de9a4dbfb2fddfc4aba81a9a
Shakibul Hasan <hasan29shakib@gmail.com>
2022-10-30 00:06:53 +0600

    codeforces:daily solved

---
+++ codeforce/factorise_n_add_m.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file factorise_n_add_m.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-29
+ * 
+ * @copyright Copyright (c) 2022
+ * problem : https://codeforces.com/contest/1740/problem/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        if (n > 2) {
+            cout<<"3"<<endl;
+        }
+        else {
+            cout<<"2"<<endl;
+        }
+    }
+}
