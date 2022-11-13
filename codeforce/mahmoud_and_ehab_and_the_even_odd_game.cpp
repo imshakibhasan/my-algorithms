032f7b34b390566d95a34d4ad0d624fb864fe565
Shakibul Hasan <hasan29shakib@gmail.com>
2022-11-13 19:43:20 +0600

    daily solvel

---
+++ codeforce/mahmoud_and_ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file mahmud_&_ehab_&_the_even_odd_game.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-13
+ * 
+ * @copyright Copyright (c) 2022
+ * problem : https://codeforces.com/problemset/problem/959/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int n;
+    cin>>n;
+    if (n%2 == 0) {
+        cout<<"Mahmoud"<<endl;
+    }
+    else {
+        cout<<"Ehab"<<endl;
+    }
+}
