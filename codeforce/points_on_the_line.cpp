74c5bad38c9c52e3640bd2a26c89bc362bf4ac03
Shakibul Hasan <hasan29shakib@gmail.com>
2022-11-13 19:46:36 +0600

    daily solvel

---
+++ codeforce/points_on_the_line.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file points_on_the_line.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-24
+ * 
+ * @copyright Copyright (cpp) 2022
+ * problem : https://codeforces.com/problemset/problem/940/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, d, i;
+    cin>>n>>d;
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+}
