05c5981ad5613fd25428bb4be914f8436f7a8fec
Shakibul Hasan <hasan29shakib@gmail.com>
2022-11-17 18:59:52 +0600

    daily solve

---
+++ codeforce/ichihime_and_triangle.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file ichihime_and_triangle.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-17
+ * 
+ * @copyright Copyright (c) 2022
+ * problem : https://codeforces.com/problemset/problem/1337/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        long long int a , b, c, d, x , y, z;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+}
