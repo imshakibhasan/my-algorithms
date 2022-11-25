12e08a9804ff5309bbfc3ca0da5e5bfcf78e741d
Shakibul Hasan <hasan29shakib@gmail.com>
2022-11-26 00:09:08 +0600

    daily solve

---
+++ codeforce/gregor_and_cryptography.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file gregor_and_cryptography.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-25
+ * 
+ * @copyright Copyright (c) 2022
+ * problem : https://codeforces.com/problemset/problem/1549/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        long long int p;
+        cin>>p;
+        cout<<"2 "<<p-1<<endl;
+    }
+}
