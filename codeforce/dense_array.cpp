19c0acb4983696ff246c3a17349dbe8dc56ba91f
Shakibul Hasan <hasan29shakib@gmail.com>
2022-12-02 23:23:51 +0600

    daily solve

--- codeforce/dense_array.cpp
+++ codeforce/dense_array.cpp
@@ -12,9 +12,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    int t;
-    cin>>t;
-    while (t--) {
+    int test;
+    cin>>test;
+    while (test--) {
         int i, n, count = 0, a , b;
         cin>>n;
         int arr[n];
