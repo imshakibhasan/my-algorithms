d02f8f38faa8b5f974e0234534018ac34d93763d
Shakibul Hasan <hasan29shakib@gmail.com>
2022-12-05 22:41:37 +0600

    daily solve

--- codeforce/unique_bid_auction.cpp
+++ codeforce/unique_bid_auction.cpp
@@ -44,3 +44,38 @@ int main() {
         cout<<pos<<endl;
     }
 }
+/*
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int t;
+    cin>>t;
+    while (t--) {
+        long long int i, j, count = 0, pos = -1, n, value = 0, flag = 0; 
+        cin>>n;
+        long long int arr[n], res[n];
+        for (i = 0; i < n; i++) {
+            cin>>arr[i];
+            res[i] = arr[i];
+        }
+        sort(res, res+n);
+        for (i = 0; i < n; i++) {
+            count = 0;
+            for (j = 0; j < n; j++) {
+                if (res[i] == arr[j]) {
+                    count++;
+                    pos = j + 1;
+                }
+            }
+            if (count == 1) {
+                break;
+            }
+            else {
+                pos = -1;
+            }
+        }
+        cout<<pos<<endl;
+    }
+}
+*/
