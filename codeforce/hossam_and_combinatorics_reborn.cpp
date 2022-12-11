46bf6ed42604325153f75fbb978886e317954579
Shakibul Hasan <hasan29shakib@gmail.com>
2022-12-12 02:32:52 +0600

    daily solve

---
+++ codeforce/hossam_and_combinatorics_reborn.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file hossam_and_combinatories.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-11
+ * 
+ * @copyright Copyright (c) 2022
+ * problem : https://codeforces.com/contest/1771/problem/A
+ * 
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        long long int n, i, count_max = 0,count_min = 0, count = 0, min = 100000, max = 0, ans = 0;
+        cin>>n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin>>arr[i];
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        for (i = 0; i < n; i++) {
+            if (arr[i] == max) {
+                count_max++;
+            }
+            if (arr[i] == min) {
+                count_min++;
+            }
+        }
+        if (count_max != n || count_min != n) {
+            count = count_max * count_min;
+            cout<<(2*(count))<<endl;
+        }
+        else if (count_max == n || count_min == n) {
+            ans = pow(count_max, 2);
+            cout<<ans - count_max<<endl;
+        }
+    }
+}
