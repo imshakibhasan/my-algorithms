110efecf75a5cbe9e070d883b9fdbc5ccf0da780
Shakibul Hasan <hasan29shakib@gmail.com>
2022-12-19 23:02:30 +0600

    daily solve

---
+++ codeforce/a+b.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file a+b.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-19
+ * 
+ * @copyright Copyright (c) 2022
+ * problem : https://codeforces.com/contest/1772/problem/A
+ */
+#include<iostream>
+#include<iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int a, c;
+        char b;
+        cin>>a>>b>>c;
+        cout<<a+c<<endl;
+    }
+}
