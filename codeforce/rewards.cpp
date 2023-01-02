8e334dc75d4f15e205ea273da2ec9b109e08c2f1
Shakibul Hasan <hasan29shakib@gmail.com>
2023-01-02 22:29:12 +0600

    codeforces: daily solve

---
+++ codeforce/rewards.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file rewards.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-02
+ * 
+ * @copyright Copyright (c) 2023
+ * problem : https://codeforces.com/problemset/problem/448/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a1, a2, a3, a, b, b1, b2, b3, n, p = 0, q = 0, ans = 0;
+    cin>>a1>>a2>>a3;
+    cin>>b1>>b2>>b3;
+    cin>>n;
+    a = a1 + a2 + a3;
+    b = b1 + b2 + b3;
+    p = ceil(a / 5.0);
+    q = ceil(b / 10.0);
+    ans = p + q;
+    if (ans <= n) {
+        cout<<"YES"<<endl;
+    }
+    else {
+        cout<<"NO"<<endl;
+    }
+}
