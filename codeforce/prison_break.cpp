7e24192ced5723e099a97ebbe85496423492a094
Shakibul Hasan <hasan29shakib@gmail.com>
2023-01-03 23:50:52 +0600

    codeforces: daily solve

---
+++ codeforce/prison_break.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file prison_break.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-03
+ * 
+ * @copyright Copyright (c) 2023
+ * problem : https://codeforces.com/problemset/problem/1482/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int a, b;
+        cin>>a>>b;
+        cout<<a*b<<endl;
+    }
+}
