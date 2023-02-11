e030384a8b7f6ca09cf271b42b5fd636bc9c5df8
Shakibul Hasan <hasan29shakib@gmail.com>
2023-02-11 21:28:27 +0600

    codeforces: daily solve

---
+++ codeforce/mezo_playing_zoma.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file mezo_playing_zoma.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-11
+ * 
+ * @copyright Copyright (c) 2023
+ * problem : https://codeforces.com/problemset/problem/1285/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, L = 0, R = 0;
+    cin>>n;
+    char arr[n];
+    cin>>arr;
+    cout<<n+1<<endl;
+}
+
+/*Input
+4
+LRLR
+*/
+
+/*Output
+5*/
