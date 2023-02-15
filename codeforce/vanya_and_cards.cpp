bc1944c52a8b8d1f7e8ececa98527f56c4c2f7bb
Shakibul Hasan <hasan29shakib@gmail.com>
2023-02-16 00:46:03 +0600

    codeforces: daily solve

--- codeforce/vanya_and_cards.cpp
+++ codeforce/vanya_and_cards.cpp
@@ -6,7 +6,7 @@
  * @date 2023-02-15
  * 
  * @copyright Copyright (c) 2023
- * 
+ * problem : https://codeforces.com/problemset/problem/401/A
  */
 #include<iostream>
 #include<bits/stdc++.h>
@@ -24,18 +24,32 @@ int main() {
     sum=abs(sum);
 
     if(sum==0) {
-        cout<<"0";
+        cout<<"0"<<endl;
     }
 
     else if(sum<=x) {
-        cout<<"1";
+        cout<<"1"<<endl;
     }
     else {
         if(sum%x==0) {
-            cout<<sum/x;
+            cout<<sum/x<<endl;
         }
         else {
-            cout<<(sum/x+1);
+            cout<<(sum/x+1)<<endl;
         }
     }
 }
+
+/*Input
+3 2
+-1 1 2
+
+2 3
+-2 -2
+*/
+
+/*Output
+1
+
+2
+*/
