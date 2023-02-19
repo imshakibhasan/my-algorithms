e0d7a2c3c8882eaed4b2edd711a5087f20f66760
Shakibul Hasan <hasan29shakib@gmail.com>
2023-02-19 22:36:52 +0600

    codeforces: daily solve

---
+++ codeforce/minority.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file minority.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-24
+ * 
+ * @copyright Copyright (c) 2023
+ * problem : https://codeforces.com/problemset/problem/1633/B
+ */
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    
+    cin>>t;
+    while(t--) {
+        int i, z , one;
+        string s;
+        z=0,one=0;
+        cin>>s;
+        for(i=0; i<s.size(); i++) {
+            if(s[i]=='0')
+                z++;
+             else
+            one++;
+        }
+        if(z!=one) {
+            cout<<min(z,one)<<endl;
+        }
+        else {
+            cout<<z-1<<endl;
+        }
+    }
+}
+/*
+Input
+4
+01
+1010101010111
+00110001000
+1
+*/
+
+/*
+Output
+0
+5
+3
+0
+*/
