e0a4980156eb186890826ba0c11adb0c3993fc23
Shakibul Hasan <hasan29shakib@gmail.com>
2023-03-03 20:17:36 +0600

    codeforces: daily solve

---
+++ codeforce/quirky_quentifiers.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file quirky_quentifiers.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-03
+ * 
+ * @copyright Copyright (c) 2023
+ * problem : https://codeforces.com/problemset/problem/952/A
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, ans;
+    cin>>a;
+    ans = a % 2;
+    cout<<ans<<endl;
+}
+
+/*
+Input
+13
+
+927
+
+48
+
+*/
+
+/*
+Ouput
+
+1
+
+1
+
+0
+
+*/
