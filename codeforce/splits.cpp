860ba51b4e07b77074486b6e486fd2fe42af7a25
Shakibul Hasan <hasan29shakib@gmail.com>
2023-03-17 15:21:25 +0600

    codeforces: daily solve

---
+++ codeforce/splits.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file splits.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-17
+ * 
+ * @copyright Copyright (c) 2023
+ * problem : https://codeforces.com/problemset/problem/964/A
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int i, n,ans = 1;
+    cin>>n;
+    ans = ans + (n/2);
+    cout<<ans<<endl;
+}
+
+/*
+Input
+
+7
+
+8
+
+9
+
+*/
+
+/*
+Output
+
+4
+
+5
+
+5
+
+*/
