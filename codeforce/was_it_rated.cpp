b6690d3046fb675a5a00d567f42b5a89c19c5225
Shakibul Hasan <hasan29shakib@gmail.com>
2023-04-06 23:47:57 +0600

    codeforces: daily solve

---
+++ codeforce/was_it_rated.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file was_it_rated.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-06
+ * 
+ * @copyright Copyright (c) 2023
+ * Problem : https://codeforces.com/problemset/problem/1812/B
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if (n == 15 || n == 21) {
+        cout<<"NO"<<endl;
+    }
+    else {
+        cout<<"YES"<<endl;
+    }
+}
