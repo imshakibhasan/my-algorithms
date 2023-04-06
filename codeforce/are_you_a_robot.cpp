4bd9484ebb1b7c46ebb56e6428b6134d549ba4c7
Shakibul Hasan <hasan29shakib@gmail.com>
2023-04-06 23:48:35 +0600

    codeforces: daily solve

---
+++ codeforce/are_you_a_robot.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file are_you_a_robot.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-06
+ * 
+ * @copyright Copyright (c) 2023
+ * Problem : https://codeforces.com/problemset/problem/1812/A
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    cout<<"security"<<endl;
+}
