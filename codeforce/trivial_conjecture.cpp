5209cd24964000a8045bfbbdc6521a2b0903100c
Shakibul Hasan <hasan29shakib@gmail.com>
2023-04-07 22:37:01 +0600

    codeforces: daily solve

---
+++ codeforce/trivial_conjecture.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file trivial_conjecture.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-07
+ * 
+ * @copyright Copyright (c) 2023
+ * Problem : https://codeforces.com/problemset/problem/1812/D
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int n;
+    cin>>n;
+    cout<<"-1"<<endl;
+}
