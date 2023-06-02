42f52aebd9dfab3bbf47653e3a03a95024461afd
Shakibul Hasan <hasan29shakib@gmail.com>
2023-06-02 23:04:55 +0600

    codeforces: daily solve

---
+++ codeforce/best_binary_string.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file best_binary_string.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-02
+ * 
+ * @copyright Copyright (c) 2023
+ * Problem :
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+        string s;
+        cin>>s;
+        char c = '0';
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '?') {
+                s[i] = c;
+            }
+            else {
+                c = s[i];
+            }
+        }
+        cout<<s<<endl;	
+    }
+}
+
+/*Input
+4
+??01?
+10100
+1??10?
+0?1?10?10
+*/
+
+/*
+Output
+00011
+10100
+111101
+011110010
+*/
