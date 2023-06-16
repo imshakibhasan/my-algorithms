7c0d13cba150d08b4e640aa619c941c782b62353
Shakibul Hasan <hasan29shakib@gmail.com>
2023-06-16 22:39:20 +0600

    codeforces: daily solve

---
+++ codeforce/alex_and_rhombus.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file alex_and_rhombus.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-16
+ * 
+ * @copyright Copyright (c) 2023
+ * Problem : https://codeforces.com/problemset/problem/1180/A
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    cout<<n*n+(n-1)*(n-1)<<endl;
+}
+
+/*Input
+1
+
+2
+
+3
+*/
+
+/*Output
+1
+
+5
+
+13
+*/
