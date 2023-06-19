b1f0bb702f3a8d12938453ea11537bb69eabedc3
Shakibul Hasan <hasan29shakib@gmail.com>
2023-06-19 22:29:02 +0600

    codeforces: daily solve

---
+++ codeforce/nastia_and_nearly_good_numbers.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file nastia_and_nearly_good_numbers.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-18
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        long long a, b;
+        cin>>a>>b;
+        if(b == 1) {
+            cout<<"NO"<<endl;
+        }
+        else {
+            cout<<"YES"<<endl<<a<<' '<<a*b<<' '<<a*(b+1)<<endl;
+        }
+    }
+}
+
+/*Input
+3
+5 3
+13 2
+7 11
+*/
+
+/*Output
+YES
+10 50 60
+YES
+169 39 208
+YES
+28 154 182
+*/
