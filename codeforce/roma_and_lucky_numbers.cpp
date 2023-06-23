59a5f1b6ff416880ae38623ce4ac97fc7e724f3f
Shakibul Hasan <hasan29shakib@gmail.com>
2023-06-23 23:30:55 +0600

    codeforces: daily solve

---
+++ codeforce/roma_and_lucky_numbers.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file roma_and_lucky_numbers.cpp
+ * @author Shakib Hasan (hasan29shakib@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-23
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include<bits/stdc++.h>
+int k,m,n,p,s;
+int main() {
+    for(scanf("%d%d",&n,&k);n--;s+=m<=k)
+    for(m=!scanf("%d",&p);p;p/=10)m+=p%10==4|p%10==7;
+    printf("%d",s);
+}
+
+/*Input
+3 4
+1 2 4
+
+3 2
+447 44 77
+*/
+
+/*Output
+3
+
+2
+
+*/
